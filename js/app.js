df13cabf33a496340b8cacd615e6515366875417
Zitle-Nancy <ancizj@hotmail.com>
2018-01-04 12:40:49 -0600

    agrega un interval a las funciones

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 (function() {
 	var app = angular.module('btc', ['testService','ngRoute']);
 
-	app.controller('getData',['$scope','testRequest', function($scope,testRequest) {
+	app.controller('getData',['$scope','testRequest','$interval', function($scope,testRequest,$interval) {
 		function getPriceVolabit() {
 			testRequest.getDataVolabit().then(function(response) {
 				$scope.priceUsdvolabit = Number(response.data.btc_usd_buy);
 				$scope.priceMxnvolabit = response.data.btc_mxn_buy;
-				getPercentage();			
 			})
 		}
-		getPriceVolabit();
-
+		
 		function getPriceBitso () {
 			testRequest.getDataBitso().then(function(response) {
 				// console.log(response.data.payload);
 			})
 		};
-		// getPriceBitso();
 
 		function getPriceBitstamp() {
 			testRequest.getDataBitstamp().then(function(response) {
@@ -24,20 +21,22 @@
 				$scope.priceMxnBitstamp = (Number($scope.priceUsdBitstamp) * $scope.mxn).toFixed(2);
 			})
 		};
-		getPriceBitstamp();
-
+		
 		function getPriceMxn() {
 			testRequest.getPriceMxn().then(function(response) {
 				$scope.mxn = response.data.rates.MXN;
 			})
 		};
-		getPriceMxn();
-
 		function getPercentage() {
 			var  difference = Number($scope.priceMxnvolabit) - Number($scope.priceMxnBitstamp);
 			var divideTheDifference = difference / Number($scope.priceMxnBitstamp);
 			$scope.percentage =  (divideTheDifference * 100).toFixed(2);
 		};
+
+		$interval(getPriceVolabit,100);
+		$interval(getPriceBitstamp,100);
+		$interval(getPriceMxn,100);
+		$interval(getPercentage,100);	
 	}]);
 
 })();
